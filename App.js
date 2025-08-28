20d72ce8a4b3818a8f0f88155ece334ff3166997
samuelbrag4 <samuelbraga.tds2.senai@gmail.com>
2025-08-28 14:00:55 -0300

    first commit

---
+++ App.js
@@ -0,0 +1,5 @@
+import React, { useState, useEffect } from 'react';
+import { View, Text, TextInput, Button, StyleSheet } from 'react-native';
+import AsyncStorage from '@react-native-async-storage/async-storage';
+import { StatusBar } from 'react-native-web';
+
