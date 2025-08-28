e1815172659671b019afd5d8e389cee83fd25eb2
samuelbrag4 <samuelbraga.tds2.senai@gmail.com>
2025-08-28 14:15:16 -0300

    feat: adds functionality to search for name saved in AsyncStorage when starting the app

--- App.js
+++ App.js
@@ -3,3 +3,22 @@ import { View, Text, TextInput, Button, StyleSheet } from 'react-native';
 import AsyncStorage from '@react-native-async-storage/async-storage';
 import { StatusBar } from 'react-native-web';
 
+export default function App() {
+
+  // Estados: o que o usuário digita e o nome salvo.
+  const [ name, setNome ] = useState('');
+  const [ saveName, setSaveName ] = useState('');
+
+  // Busca o nome d=salvo quando o app abre.
+  useEffect(() => {
+    async function getName() {
+      const savedName = await AsyncStorage.getItem('nameUser');
+      if (savedName) {
+        setSaveName(savedName);
+      }
+    }
+    getName();
+  }, []);
+
+  
+}
